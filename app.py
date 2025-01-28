0550e5b09efd84a62c8115667047163666c7e06d
Zhihan Zhu <zhihan.zhu@swisstph.ch>
2025-01-28 14:58:33 +0100

    update text reading, editing and saving

--- app.py
+++ app.py
@@ -1,19 +1,62 @@
-# Import packages
-from dash import Dash, html, dash_table
-import pandas as pd
+import dash
+from dash import dcc, html, Input, Output, State, callback, no_update
+from dash.exceptions import PreventUpdate
+import base64
 
-# Incorporate data
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
+app = dash.Dash(__name__)
 
-# Initialize the app
-app = Dash()
+app.layout = html.Div([
+    # File upload component
+    dcc.Upload(
+        id='upload-txt',
+        children=html.Button('Upload TXT File'),
+        multiple=False
+    ),
+    
+    # Text area for editing
+    dcc.Textarea(
+        id='text-editor',
+        style={'width': '100%', 'height': '400px', 'margin': '20px 0'}
+    ),
+    
+    # Save button
+    html.Button('Save Changes', id='save-button'),
+    
+    # Hidden download component
+    dcc.Download(id='download-modified-txt')
+])
 
-# App layout
-app.layout = [
-    html.Div(children='My First App with Data'),
-    dash_table.DataTable(data=df.to_dict('records'), page_size=10)
-]
+# Handle file upload and populate textarea
+@callback(
+    Output('text-editor', 'value'),
+    Input('upload-txt', 'contents'),
+    State('upload-txt', 'filename')
+)
+def load_txt_file(contents, filename):
+    if contents is None:
+        raise PreventUpdate
+    
+    # Decode TXT file
+    content_type, content_string = contents.split(',')
+    decoded = base64.b64decode(content_string).decode('utf-8')
+    return decoded  # Populate textarea
 
-# Run the app
+# Save modified content as new TXT file
+@callback(
+    Output('download-modified-txt', 'data'),
+    Input('save-button', 'n_clicks'),
+    State('text-editor', 'value'),
+    prevent_initial_call=True
+)
+def save_modified_txt(n_clicks, modified_content):
+    if not modified_content:
+        raise PreventUpdate
+    
+    # Create downloadable TXT file
+    return dict(
+        content=modified_content,
+        filename="modified_file.txt"
+    )
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run_server(debug=True)
